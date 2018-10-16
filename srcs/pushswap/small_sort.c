605522ae038038aea10ea1a970e208d51c5b2994
William Braeckman <wbraeckm@student.s19.be>
2018-10-16 14:38:05 +0200

    Changement

--- srcs/pushswap/small_sort.c
+++ srcs/pushswap/small_sort.c
@@ -6,20 +6,21 @@
 /*   By: wbraeckm <wbraeckm@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/10/09 23:55:07 by wbraeckm          #+#    #+#             */
-/*   Updated: 2018/10/10 00:58:42 by wbraeckm         ###   ########.fr       */
+/*   Updated: 2018/10/16 14:30:05 by wbraeckm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pushswap.h"
 
-void	ft_sort_2(t_ps *ps) {
+void	ft_sort_2(t_ps *ps)
+{
 	if (ps->pile_a[ft_index(ps, 0, PILE_A)]
 		> ps->pile_a[ft_index(ps, 1, PILE_A)])
 		ft_sa(ps);
 }
 
-//2 1 3 | 1 3 2 | 3 2 1
-void	ft_sort_3(t_ps *ps) {
+void	ft_sort_3(t_ps *ps)
+{
 	ft_check_rotation_only(ps);
 	if (ft_is_ordered(ps))
 		return ;
@@ -40,7 +41,8 @@ void	ft_sort_3(t_ps *ps) {
 	}
 }
 
-void	ft_sort_4(t_ps *ps) {
+void	ft_sort_4(t_ps *ps)
+{
 	int i;
 
 	i = ft_find_smallest(ps);
@@ -52,5 +54,25 @@ void	ft_sort_4(t_ps *ps) {
 			ft_rra(ps);
 	ft_pb(ps);
 	ft_sort_3(ps);
+	ft_pa(ps);
+}
+
+/*
+** TODO: Maybe check for second smallest while ra/rra-ing
+*/
+
+void	ft_sort_5(t_ps *ps)
+{
+	int i;
+
+	i = ft_find_smallest(ps);
+	if (i <= ps->size_a / 2)
+		while (i--)
+			ft_ra(ps);
+	else
+		while (i++ < ps->size_a)
+			ft_rra(ps);
+	ft_pb(ps);
+	ft_sort_4(ps);
 	ft_pa(ps);
 }
