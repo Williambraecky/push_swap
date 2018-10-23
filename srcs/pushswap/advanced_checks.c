ff990034043929f1d628baf5ea98d2421264ec87
William Braeckman <wbraeckm@student.s19.be>
2018-10-23 15:51:24 +0200

    Update algorithm

--- srcs/pushswap/advanced_checks.c
+++ srcs/pushswap/advanced_checks.c
@@ -6,7 +6,7 @@
 /*   By: wbraeckm <wbraeckm@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/09/27 12:19:51 by wbraeckm          #+#    #+#             */
-/*   Updated: 2018/10/16 14:55:10 by wbraeckm         ###   ########.fr       */
+/*   Updated: 2018/10/23 15:46:44 by wbraeckm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -76,7 +76,7 @@ void	ft_find_smartest_move(t_ps *ps, int movs[5])
 	movs[0] = LIB_INT_MAX;
 	movs[1] = LIB_INT_MAX;
 	i = 0;
-	while (i <= ps->size_a / 2 && i <= ps->size_b / 2 && movs[1] > i)
+	while (i <= ps->size_a / 2 && i <= ps->size_b / 2)
 	{
 		movs[2] = ft_calc_position(ps, ps->pile_a[ft_index(ps, i, PILE_A)]);
 		movs[3] = ft_calc_movements(ps, i, movs[2]);
